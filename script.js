d5435d3cc40676ecc47b39faaf086b3ca4176cbc
Diwakar Verma <88948065+EpicDivakar@users.noreply.github.com>
2025-06-28 23:27:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,7 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
